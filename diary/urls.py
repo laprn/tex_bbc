0d69a643e015a3d2c1164413b13984948036d5b0
laprn <reg@dymcom.net>
2020-11-09 17:45:51 +0900

    diary_list.htmlを作成中

--- diary/urls.py
+++ diary/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'diary'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index')
+    path('', views.IndexView.as_view(), name='index'),
+    path('inquiry/', views.InquiryView.as_view(), name='inquiry'),
+    path('diary-list/', views.DiaryListView.as_view(), name='diary_list')
 ]
--- diary/views.py
+++ diary/views.py
@@ -1,4 +1,15 @@
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Diary
 
 class IndexView(generic.TemplateView):
     template_name = 'index.html'
+
+class DiaryListView(LoginRequiredMixin, generic.ListView):
+    model = Diary
+    template_name = 'diary_list.html'
+
+    def get_queryset(self):
+        diaries = Diary.object.filter(user=self.request.user).order_by('-created-at')
+        return diaries
